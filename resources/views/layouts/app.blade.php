530ddc61b299b55f177413a4d26eb07c8252cbfe
Sharifah Asilah <asilah2110@gmail.com>
2020-12-17 01:09:40 +0800

    Run composer update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
                     <div><img src="/svg/freeCodeCampLogo.svg" style="border-right: 1px solid #1b1e21; height: 20px" class="pr-3"/></div>
-                    <div class="pl-3 pt-1">freeCodeGram</div>
+                    <div class="pl-3 pt-1">SharifahAsilah</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
